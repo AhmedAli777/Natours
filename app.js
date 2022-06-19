32587f2709538dd22bcc781b92fddfc15a515afe
Ahmed Ali <ahmali777@gmail.com>
2022-06-19 02:40:29 +0200

    improvd stripe implementation

--- app.js
+++ app.js
@@ -62,11 +62,11 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 //
-// app.post(
-//   '/webhook-checkout',
-//   bodyParser.raw({ type: 'application/json' }),
-//   bookingController.webhookCheckout
-// );
+app.post(
+  '/webhook-checkout',
+  bodyParser.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 //Body barser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
