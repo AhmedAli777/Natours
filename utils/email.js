50c7f525d3b706be8003f9e8daa039c3ed827f76
Ahmed Ali <ahmali777@gmail.com>
2022-06-21 23:36:06 +0200

    improved mail sittings

--- utils/email.js
+++ utils/email.js
@@ -14,10 +14,10 @@ module.exports = class Email {
     if (process.env.NODE_ENV === 'production') {
       // Sendgrid
       return nodemailer.createTransport({
-        service: 'SendGrid',
+        service: 'SendPulse',
         auth: {
-          user: process.env.SENDGRID_USERNAME,
-          pass: process.env.SENDGRID_PASSWORD,
+          user: process.env.SENDPLUSE_USERNAME,
+          pass: process.env.SENDPLUSE_PASSWORD,
         },
       });
     }
